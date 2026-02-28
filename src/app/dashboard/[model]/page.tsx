b5151b6760e9c52ae6296a6704bfbb3e7810ebab
om kasana <omkasana19@gmail.com>
2026-02-28 14:55:33 +0530

    modularise

--- src/app/dashboard/[model]/page.tsx
+++ src/app/dashboard/[model]/page.tsx
@@ -7,13 +7,12 @@ interface Props {
 
 export default async function ModulePage({ params }: Props) {
   const { model } = await params;
-  console.log("model", model);
+
   const config = modulesRegistry[model];
 
   if (!config) {
     return <div className="p-6">Module not found — param: {model}</div>;
   }
 
-  console.log("config", config);
   return <DynamicModule config={config} />;
 }
